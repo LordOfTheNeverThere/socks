86aff719c52e42caa7e75e9b6d906e94f7c057fc
LordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2026-02-23 13:02:41 +0000

    Add exception unittests

--- tests/NetworkListener.cpp
+++ tests/NetworkListener.cpp
@@ -16,6 +16,16 @@ TEST(MethodChecking, LocalhostResponse) {
 }
 
 
+TEST(MethodChecking, LocalhostResponseError) {
+    NetworkListener socket;
+    try {
+        socket.fetchInterfacesToBind("8080", 7, 0);
+    } catch (std::string error) {
+        EXPECT_EQ(error, "The name resolution was not able to find any viable interfaces for the socket connection to localhost on port: 8080 and IP version: 7.\nPrompting the error with status code: ai_family not supported");
+    }
+}
+
+
 TEST(MethodChecking, RemoteHostResponse) {
 
     NetworkListener socket;
@@ -26,5 +36,16 @@ TEST(MethodChecking, RemoteHostResponse) {
         EXPECT_TRUE(interface.family == AF_INET6);
         EXPECT_TRUE(interface.socketType == SOCK_DGRAM || interface.socketType == SOCK_STREAM || interface.socketType == SOCK_RAW );
         EXPECT_TRUE(interface.ipString == "::1"); // localhost ipv6
+    }
+}
+
+
+TEST(MethodChecking, RemoteHostResponseError) {
+
+    NetworkListener socket;
+    try {
+        socket.fetchInterfacesToConnect("myata","65000", AF_INET6, 0);
+    } catch (std::string error) {
+        EXPECT_EQ(error, "The name resolution was not able to find any viable interfaces for the socket connection to myata on port: 65000 and IP version: 10.\nPrompting the error with status code: Temporary failure in name resolution");
     }
 }
